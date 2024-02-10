cf51846039c2ef0d35f4dd1133c1e00f6deb6cde
gabrielQ12 <gabriel.quinette1@gmail.com>
2024-02-10 15:33:22 +0100

    page:DONE

--- MySneakers/ViewController.swift
+++ MySneakers/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameTF: UITextField!
+    
+    @IBOutlet weak var typeSegmented: UISegmentedControl!
+    
+    @IBOutlet weak var genderSwitch: UISwitch!
+    
+    @IBOutlet weak var sizeStepper: UIStepper!
+    
+    @IBOutlet weak var colorPicker: UIPickerView!
+    
+    @IBOutlet weak var resultIV: UIImageView!
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func segmentedChanged(_ sender: UISegmentedControl) {
+    }
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+    }
+    
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+    }
+    
 }
 
